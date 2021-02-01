afe775f6c3714efa1415b77bf292d9e34732d50f
Ivar Conradi Østhus <ivarconr@gmail.com>
2021-02-01 11:42:38 +0100

    fix: failing test

--- test/e2e/api/admin/addon.e2e.test.js
+++ test/e2e/api/admin/addon.e2e.test.js
@@ -26,7 +26,7 @@ test.serial('gets all addons', async t => {
         .expect(200)
         .expect(res => {
             t.is(res.body.addons.length, 0, 'expected 0 configured addons');
-            t.is(res.body.providers.length, 1, 'expected 1 addon provider');
+            t.is(res.body.providers.length, 2, 'expected 1 addon provider');
             t.is(res.body.providers[0].name, 'webhook');
         });
 });
